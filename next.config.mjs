b038eac2b06740cd7035f7abde7b862b81798a45
Muhamed Musa <muhamed.musa1994@gmail.com>
2025-07-26 23:27:20 +0200

    process polish

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "**",
-            },
-        ],
+  reactStrictMode: false,
+  experimental: {
+    optimizePackageImports: ['@iconify/react', 'framer-motion'],
+  },
+  turbopack: {
+    resolveAlias: {
+      '@': './src',
+      '#': '.',
     },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+  compiler: {
+    styledComponents: true,
+  },
 };
 
 export default nextConfig;
